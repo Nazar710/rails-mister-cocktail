6e51b445eeb543567c5b5b7a486c7631d2cb8af9
evgeny nazarenko <nazarenkoe@me.com>
2019-08-09 17:08:04 +0200

    cocktail with photo uploader

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,6 @@
 class CocktailsController < ApplicationController
+
+
   def index
   @cocktails = Cocktail.all
   end
@@ -19,9 +21,11 @@ class CocktailsController < ApplicationController
       render :new
     end
   end
+
+
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,10 @@
+class PhotoUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  include Cloudinary::CarrierWave
+
+  # Remove everything else
+end
